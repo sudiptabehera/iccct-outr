6d4a2f7543e53d6dd2c1d7bf0e4db5359b4395aa
sudiptabehera <sudipta.behera@outlook.com>
2022-07-03 00:06:12 +0530

    committee complete

--- src/containers/Committee/InternationalCommitee.js
+++ src/containers/Committee/InternationalCommitee.js
@@ -8,9 +8,9 @@ function StepC() {
           <table cellpadding="0" cellspacing="0" border="0">
             <thead>
               <tr>
-                <th style={{ width: "9%", textAlign: "left" }}>Sl.No</th>
-                <th style={{ width: "15%" }}>NAME</th>
-                <th style={{ width: "30%" }}>UNIVERSITY/COLLEGE)</th>
+                <th style={{ width: "10%", textAlign: "left" }}>Sl.No</th>
+                <th style={{ width: "20%" }}>NAME</th>
+                <th style={{ width: "25%" }}>UNIVERSITY /COLLEGE)</th>
                 <th style={{ textAlign: "center", width: "15%" }}>DEPT</th>
                 <th style={{ width: "28%" }}>MAILID</th>
               </tr>
@@ -22,15 +22,15 @@ function StepC() {
             <tbody>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>1</td>
-                <td style={{ width: "15%" }}>Prof. Saraju Mohanty</td>
-                <td style={{ width: "30%" }}>North Texas University , USA</td>
+                <td style={{ width: "20%" }}>Prof. Saraju Mohanty</td>
+                <td style={{ width: "25%" }}>North Texas University , USA</td>
                 <td style={{ textAlign: "center", width: "15%" }}>CSE</td>
                 <td style={{ width: "28%" }}></td>
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>2</td>
-                <td style={{ width: "15%" }}>Dr.Pushpita Chatterjee</td>
-                <td style={{ width: "30%" }}>
+                <td style={{ width: "20%" }}>Dr. Pushpita Chatterjee</td>
+                <td style={{ width: "25%" }}>
                   
                   Tennessee State University,USA
                 </td>
@@ -39,15 +39,15 @@ function StepC() {
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>3</td>
-                <td style={{ width: "15%" }}>Dr.Uttam Ghosh</td>
-                <td style={{ width: "30%" }}>Meharry medical collage,USA</td>
+                <td style={{ width: "20%" }}>Dr. Uttam Ghosh</td>
+                <td style={{ width: "25%" }}>Meharry medical collage,USA</td>
                 <td style={{ textAlign: "center", width: "15%" }}></td>
                 <td style={{ width: "28%" }}></td>
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>4</td>
-                <td style={{ width: "15%" }}>Dr.Paolo Barsocchi</td>
-                <td style={{ width: "30%" }}>
+                <td style={{ width: "20%" }}>Dr. Paolo Barsocchi</td>
+                <td style={{ width: "25%" }}>
                   Institute of Information Science and Technologies, National
                   Research Council,56124 Pisa,Italy
                 </td>
@@ -56,8 +56,8 @@ function StepC() {
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>5</td>
-                <td style={{ width: "15%" }}>Prof. Muhammad Fazal Ijaz</td>
-                <td style={{ width: "30%" }}>
+                <td style={{ width: "20%" }}>Prof. Muhammad Fazal Ijaz</td>
+                <td style={{ width: "25%" }}>
                   Sejong University,Seoul 05006,Korea
                 </td>
                 <td style={{ textAlign: "center", width: "15%" }}>
@@ -67,8 +67,8 @@ function StepC() {
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>6</td>
-                <td style={{ width: "15%" }}>Prof. Dulani Meedeniya</td>
-                <td style={{ width: "30%" }}>
+                <td style={{ width: "20%" }}>Prof. Dulani Meedeniya</td>
+                <td style={{ width: "25%" }}>
                   University of Moratuwa,Sri Lanka
                 </td>
                 <td style={{ textAlign: "center", width: "15%" }}>
@@ -78,8 +78,8 @@ function StepC() {
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>7</td>
-                <td style={{ width: "15%" }}>Dr. Thomas Heinrich Musiolik</td>
-                <td style={{ width: "30%" }}>
+                <td style={{ width: "20%" }}>Dr. Thomas Heinrich Musiolik</td>
+                <td style={{ width: "25%" }}>
                   Berlin university of the Art (UDK Berlin)
                 </td>
                 <td style={{ textAlign: "center", width: "15%" }}>
@@ -89,8 +89,8 @@ function StepC() {
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>8</td>
-                <td style={{ width: "15%" }}>Prof. K. G. Subramanian</td>
-                <td style={{ width: "30%" }}>Liverpool Hope University UK</td>
+                <td style={{ width: "20%" }}>Prof. K. G. Subramanian</td>
+                <td style={{ width: "25%" }}>Liverpool Hope University UK</td>
                 <td style={{ textAlign: "center", width: "15%" }}>
                   Mathematics Computer Science and Engineering
                 </td>
@@ -98,9 +98,9 @@ function StepC() {
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>9</td>
-                <td style={{ width: "15%" }}>Prof. Corina Rotar</td>
+                <td style={{ width: "20%" }}>Prof. Corina Rotar</td>
 
-                <td style={{ width: "30%" }}>University Alba Lulia Romania</td>
+                <td style={{ width: "25%" }}>University Alba Lulia Romania</td>
                 <td style={{ textAlign: "center", width: "15%" }}>
                   Computer science
                 </td>
@@ -108,9 +108,9 @@ function StepC() {
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>10</td>
-                <td style={{ width: "15%" }}>Prof. Mitra Foulandirad</td>
+                <td style={{ width: "20%" }}>Prof. Mitra Foulandirad</td>
 
-                <td style={{ width: "30%" }}>
+                <td style={{ width: "25%" }}>
                   Aix Marseille University France
                 </td>
                 <td style={{ textAlign: "center", width: "15%" }}></td>
@@ -118,9 +118,9 @@ function StepC() {
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>11</td>
-                <td style={{ width: "15%" }}>Prof. Frank Coolen</td>
+                <td style={{ width: "20%" }}>Prof. Frank Coolen</td>
 
-                <td style={{ width: "30%" }}>
+                <td style={{ width: "25%" }}>
                   Director of Postgraduate Studies,Durham University UK
                 </td>
                 <td style={{ textAlign: "center", width: "15%" }}>
@@ -130,9 +130,9 @@ function StepC() {
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>12</td>
-                <td style={{ width: "15%" }}>Prof. Xiao-Zhi Gao</td>
+                <td style={{ width: "20%" }}>Prof. Xiao-Zhi Gao</td>
 
-                <td style={{ width: "30%" }}>
+                <td style={{ width: "25%" }}>
                   School of Computing , University of Estern Finland , FI-70211,
                   Kuopio, Finland
                 </td>
@@ -141,97 +141,97 @@ function StepC() {
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>13</td>
-                <td style={{ width: "15%" }}>Dr. DB Todd</td>
-                <td style={{ width: "30%" }}>Jr Blvd,Nashville, USA</td>
+                <td style={{ width: "20%" }}>Dr. DB Todd</td>
+                <td style={{ width: "25%" }}>Jr Blvd,Nashville, USA</td>
                 
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>14</td>
-                <td style={{ width: "15%" }}>Dr. Nachiketa Mishra</td>
-                <td style={{ width: "30%" }}>IIITDM, Kanchipuram</td>
+                <td style={{ width: "20%" }}>Dr. Nachiketa Mishra</td>
+                <td style={{ width: "25%" }}>IIITDM, Kanchipuram</td>
                 <td style={{ textAlign: "center", width: "15%" }}>Math</td>
                 <td style={{ width: "28%" }}>nmishra@iiitdm.ac.in</td>
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>15</td>
-                <td style={{ width: "15%" }}>Dr. B. D. Sahu</td>
-                <td style={{ width: "30%" }}>NIT, Rourkela</td>
+                <td style={{ width: "20%" }}>Dr. B. D. Sahu</td>
+                <td style={{ width: "25%" }}>NIT, Rourkela</td>
                 <td style={{ textAlign: "center", width: "15%" }}>CSE</td>
                 <td style={{ width: "28%" }}>rampadhy@iiitdm.ac.in</td>
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>16</td>
-                <td style={{ width: "15%" }}>Dr. Srinivas Sethi</td>
-                <td style={{ width: "30%" }}>IGIT, Saranga</td>
+                <td style={{ width: "20%" }}>Dr. Srinivas Sethi</td>
+                <td style={{ width: "25%" }}>IGIT, Saranga</td>
                 <td style={{ textAlign: "center", width: "15%" }}>CSE</td>
                 <td style={{ width: "28%" }}>sanjeethn@iiitdm.ac.in</td>
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>17</td>
-                <td style={{ width: "15%" }}>Dr. Rashmi Ranjan Rout</td>
+                <td style={{ width: "20%" }}>Dr. Rashmi Ranjan Rout</td>
 
-                <td style={{ width: "30%" }}>NIT,Warangal</td>
+                <td style={{ width: "25%" }}>NIT,Warangal</td>
                 <td style={{ textAlign: "center", width: "15%" }}>CSE</td>
                 <td style={{ width: "28%" }}>rashrr@nitw.ac.in</td>
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>18</td>
-                <td style={{ width: "15%" }}>Dr.Sanyasiraju V S SYedida</td>
-                <td style={{ width: "30%" }}>IIT,Madras</td>
+                <td style={{ width: "20%" }}>Dr.Sanyasiraju V S SYedida</td>
+                <td style={{ width: "25%" }}>IIT,Madras</td>
                 <td style={{ textAlign: "center", width: "15%" }}>Math</td>
                 <td style={{ width: "28%" }}>sryedida@iitm.ac.in</td>
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>19</td>
-                <td style={{ width: "15%" }}>Dr.B. S. Panda</td>
+                <td style={{ width: "20%" }}>Dr.B. S. Panda</td>
 
-                <td style={{ width: "30%" }}>IIT,Delhi</td>
+                <td style={{ width: "25%" }}>IIT,Delhi</td>
                 <td style={{ textAlign: "center", width: "15%" }}>Math</td>
                 <td style={{ width: "28%" }}>bspanda@maths.iitd.ac.in</td>
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>20</td>
-                <td style={{ width: "15%" }}>Dr. Satyananda Panda</td>
+                <td style={{ width: "20%" }}>Dr. Satyananda Panda</td>
 
-                <td style={{ width: "30%" }}>NIT,Calicut</td>
+                <td style={{ width: "25%" }}>NIT,Calicut</td>
                 <td style={{ textAlign: "center", width: "15%" }}>Math</td>
                 <td style={{ width: "28%" }}>satyananda@nitc.ac.in</td>
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>21</td>
-                <td style={{ width: "15%" }}>Dr. Ratikanta Behera</td>
-                <td style={{ width: "30%" }}>IISC,Bangalore</td>
+                <td style={{ width: "20%" }}>Dr. Ratikanta Behera</td>
+                <td style={{ width: "25%" }}>IISC,Bangalore</td>
                 <td style={{ textAlign: "center", width: "15%" }}>CDS</td>
                 <td style={{ width: "28%" }}></td>
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>22</td>
-                <td style={{ width: "15%" }}>Dr. Soumya Ranjan Nayak</td>
-                <td style={{ width: "30%" }}>Amity University, Noida</td>
+                <td style={{ width: "20%" }}>Dr. Soumya Ranjan Nayak</td>
+                <td style={{ width: "25%" }}>Amity University, Noida</td>
                 <td style={{ textAlign: "center", width: "15%" }}></td>
                 <td style={{ width: "28%" }}>Nayak.soumya17@gmail.com</td>
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>23</td>
-                <td style={{ width: "15%" }}>Dr. M. R. Kabat</td>
+                <td style={{ width: "20%" }}>Dr. M. R. Kabat</td>
 
-                <td style={{ width: "30%" }}>VSSUT, Burla</td>
+                <td style={{ width: "25%" }}>VSSUT, Burla</td>
                 <td style={{ textAlign: "center", width: "15%" }}></td>
                 <td style={{ width: "28%" }}></td>
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>24</td>
-                <td style={{ width: "15%" }}>Dr. Muktikanta Sahu</td>
+                <td style={{ width: "20%" }}>Dr. Muktikanta Sahu</td>
 
-                <td style={{ width: "30%" }}>IIIT ,Bhubaneswar</td>
+                <td style={{ width: "25%" }}>IIIT ,Bhubaneswar</td>
                 <td style={{ textAlign: "center", width: "15%" }}></td>
                 <td style={{ width: "28%" }}></td>
               </tr>
               <tr>
                 <td style={{ width: "9%", textAlign: "left" }}>25</td>
-                <td style={{ width: "15%" }}>Dr. Debasis Gountia</td>
+                <td style={{ width: "20%" }}>Dr. Debasis Gountia</td>
 
-                <td style={{ width: "30%" }}>Assoc. Professor, CSA, OUTR</td>
+                <td style={{ width: "25%" }}>Assoc. Professor, CSA, OUTR</td>
                 <td style={{ textAlign: "center", width: "15%" }}></td>
                 <td style={{ width: "28%" }}></td>
               </tr>
